242e06cdfa6ec517167d416acaee1a9ecb19d879
Ken Aida <aidken+github@gmail.com>
2020-10-11 13:34:05 +0900

    Added three scripts to understand how typing and mypy work.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# python_type_checking
+# Python Type Checking
 Some code to understand Python type checking.
+
+### sales1.py
+A script with today's my understanding. I do not fully utilize type checking.
+
+### sales2.py
+I hear that [mypy](http://mypy-lang.org/) runs sanity check on types. I write code to set string as int
+property and mypy does not complain. Why doesn't it warn my mistake? What mypy
+offers is potential mistakes like putting a string into a property which should
+an integer?
+
+### sales3.py
+[mypy](http://mypy-lang.org/) reports erros on reduce functions. My code returns value of int or float
+correctly, but mypy thinks it is wrong.
+
+This is understandable. Real values will be known when code really executes.
+Software such as mypy cannot know what values are actually thrown to functions
+such as reduce().
+
+I added more of type hints to helper functions that reduce() uses, but mypy
+raises errors anyway.
+
+So, here is my qusetion. What is mypy for, if it does not understand code correctly. In what ways does it help users?
+
+When I define a class such as order, and I want its itemlines property to be a list of another class Itemline, I can write `List[Itemline]`, which is nice. When I do this, definition of `class Itemline` should be come before definition of `class Order`. This is understandable.
+
+
+## Conclusion
+Type hints and checking helps me become more organized about data structure I am
+trying to create.
+
+[typing package](https://docs.python.org/3/library/typing.html) "does not enforce function and variable type annotations." Still it helps me to think ahead about data structure.
+
+[mypy](http://mypy-lang.org/) seems not to be able to analyze my code correctly, although I give extensive type hints. I do not understand what [mypy](http://mypy-lang.org/) is for.
+
+
+# History
+October 11th, 2020: Written.
---
+++ sales3.py
@@ -0,0 +1,99 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import logging
+import sys
+from dataclasses import dataclass, field
+from functools import reduce
+from typing import List
+# cSpell:ignore datefmt
+
+# version with more of type hints.
+
+
+@dataclass
+class Itemline:
+    itemline_number: int
+    item_number: str
+    qty: int
+    unit_price: float
+
+    @property
+    def extended_price(self) -> float:
+        return self.unit_price * self.qty
+
+
+@dataclass
+class Order:
+    order_number: int
+    itemlines: List[Itemline] = field(default_factory=list, init=False)
+
+    def __post_init__(self):
+        self.order_number = int(self.order_number)
+
+    @property
+    def total_order_qty(self) -> int:
+        def _do_sum(a: Itemline, b: Itemline):
+            return a.qty + b.qty
+        return reduce(_do_sum, self.itemlines)
+
+    @property
+    def total_order_price(self) -> float:
+        def _do_sum(a: Itemline, b: Itemline):
+            return a.extended_price + b.extended_price
+        return reduce(_do_sum, self.itemlines)
+
+
+def main():
+    order1 = Order(order_number=1)
+    order1.itemlines.append(
+        Itemline(
+            itemline_number=10,
+            item_number='Vanilla',
+            qty=3,
+            unit_price=1.00
+        )
+    )
+    order1.itemlines.append(
+        Itemline(
+            itemline_number=20,
+            item_number='Chocolate',
+            qty=5,
+            unit_price=1.20
+        )
+    )
+
+    print(f'Total Order Qty: {order1.total_order_qty}.')
+    print(f'Total Order Price: {order1.total_order_price}.')
+
+
+def test():
+    pass
+
+
+if __name__ == '__main__':
+
+    # logger setup
+    filename = str(sys.argv[0])[:-3] + '.log'
+    format = '%(asctime)s - %(filename)s: %(lineno)s: %(funcName)s - %(levelname)-8s: %(message)s'
+    logging.basicConfig(
+        filename=filename,
+        format=format,
+        datefmt='%m-%d %H:%M',
+        level=logging.INFO,
+        # level   =logging.DEBUG,
+        # level   =logging.ERROR,
+    )
+
+    # https://docs.python.org/3/howto/logging-cookbook.html#logging-to-multiple-destinations
+    # define a Handler which writes INFO messages or higher to the sys.stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    logging.getLogger('').addHandler(console)
+
+    main()
